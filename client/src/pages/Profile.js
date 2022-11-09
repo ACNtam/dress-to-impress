bacaed9d3324ebd464ed662f17c8250bd6d766e7
ACNtam <alicecntam@gmail.com>
2022-11-09 09:26:15 -0800

    basic login button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,23 +2,26 @@ import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Profile() {
-    const { isAuthenticated } = useAuth0()
-    if (isAuthenticated) {
-        return (
-          <div>
-            Hello {user.name}{' '}
-            <button onClick={() => logout({ returnTo: window.location.origin })}>
-              Log out
-            </button>
-          </div>
-        );
-      } else {
-        return <button onClick={loginWithRedirect}>Log in</button>;
-      }
-    }
+  //Destructuring keys from useAuth0 object
+  const {
+    isAuthenticated,
+    user,
+    loginWithRedirect,
+    logout } = useAuth0()
 
-  return (
-    <div>Profile</div>
-  )
+  if (isAuthenticated) {
+    return (
+      <div>
+        Hello {user.name}{' '}
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+          Log out
+        </button>
+      </div>
+    );
+  } else {
+    return <button onClick={loginWithRedirect}>Log in</button>;
+  }
+}
+
 
 export default Profile
