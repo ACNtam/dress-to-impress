17ea090c529361a480bf9055bfe0a1791413ea0d
ACNtam <alicecntam@gmail.com>
2022-11-09 09:27:12 -0800

    fetch protected endpoint

--- client/src/pages/Protected.js
+++ client/src/pages/Protected.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 
-function Protected() {
+// async 
+async function Protected() {
     //gives token for login user
     const { getAccessTokenSilently } = useAuth0()
-    
-
+    //gives access token
+    const accessToken = await getAccessTokenSilently()
+    fetch("http://localhost:8080/protected", {
+        headers: {Authorization: `Bearer ${accessToken}`, 
+        "content-type": "application/json"
+    }   
+    }).then((res)=>{
+        return res.json() 
+    }).then((data)=>{
+        console.log(data)
+    })
+    .catch((e)=>{
+        console.log(e)
+    })
+   
+console.log(accessToken)
     return (
-        <div>Protected</div>
+        <div>
+            <h1>Protected</h1>
+            {/* <p>{accessToken}</p> */}
+        </div>
     )
 }
 
