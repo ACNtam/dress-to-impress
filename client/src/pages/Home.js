29cb1f0450a592727c03f9e13592905685911cd5
ACNtam <alicecntam@gmail.com>
2022-11-14 14:11:54 -0800

    updated component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,3 +7,35 @@ function Home() {
 }
 
 export default Home
+
+
+// import Popup from 'reactjs-popup';
+
+// const Subscribe = () => (
+//   Popup trigger={<button className="subscribe-button" Subscribe /button} modal>
+
+//     {close => (
+//       div className="subscribe"
+//         {/* This is the 'x' sign with close functionality */}
+//         button className="close" onClick={close}
+//           ×
+//         /button
+
+//         div className="subscribe-box"
+
+//           div className="subscribe-box-left"
+//             h2Never miss an update!/h2
+//             pSubscribe to your favorite blog!/p
+
+//             div className="subscribe-box-left-input"
+//               input type="email" placeholder="enter email" /buttonSubscribe/button
+//             /div
+
+//           /div
+          
+//         /div
+//       /div
+//     )}
+//   /Popup
+// );
+// export default Subscribe;
