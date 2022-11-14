0341772014b7fc2be471ef9f5ad866bf288ccc94
ACNtam <alicecntam@gmail.com>
2022-11-13 16:44:11 -0800

    added images to recommendation page

--- client/src/pages/Recommendations.js
+++ client/src/pages/Recommendations.js
@@ -25,7 +25,17 @@ function Recommendations() {
     return (
         <div>
         {recommendations?.data?.products?.map(product=>(
-            <p>{product.image.caption}</p>
+            <div key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.shortDescription}</p>
+            <p>
+                <img src={product.image.sizes[1].url} alt=""/>
+            </p>
+            <p>
+                {/* target allows to open link on new tab */}
+                <a href={product.referralUrl} rel="noreferrer" target="_blank">Buy Now</a>
+            </p>
+            </div>
         ))}
         </div>
     )
