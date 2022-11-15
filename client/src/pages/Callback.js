5a1e0ec1e0b79eb03d23a675a104724ff69b4c89
ACNtam <alicecntam@gmail.com>
2022-11-14 18:00:24 -0800

    imported imported loginRedirect component

--- client/src/pages/Callback.js
+++ client/src/pages/Callback.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useAuth0} from "@auth0/auth0-react"
+import LoginRedirect from '../component/Login-redirect'
 
 function Callback() {
     const { error } = useAuth0()
@@ -14,7 +15,7 @@ function Callback() {
 
   return (
     <div>
-        <h1>Login Success!</h1>
+        <LoginRedirect />
     </div>
   )
 }
